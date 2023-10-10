6987116867aa5fd56a5494baee849c4f2cc7ed51
Awotubo Feranmi Gabriel <feranmiawotubo16@gmail.com>
2023-10-10 01:40:34 +0100

    correctly implemented save, init, str, to_dict (all of task 3)

--- models/base_model.py
+++ models/base_model.py
@@ -30,12 +30,30 @@ class BaseModel:
         my_dict = self.__dict__
 
         return f'[{class_name}] ({id}) {my_dict}'
+    
     def save(self):
         """
         Update the public instance `updated_at` with
         current datetime
         """
         self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a comprehensive dictionary of the class
+        """
+
+        new_dict = {}
+        for x,y in self.__dict__.items():
+            new_dict.update({x: y})
+        class_name = __class__.__name__
+        created = self.created_at.isoformat()
+        updated = self.updated_at.isoformat()
+        new_dict.update({"__class__": class_name})
+        new_dict.update({"created_at": created})
+        new_dict.update({"updated_at": updated})
+
+        return new_dict
     
 if __name__ == "__main__":
     b1 = BaseModel()
