d8a0c0485e927b74fd9e263353bb3b08065c23e5
Awotubo Feranmi Gabriel <feranmiawotubo16@gmail.com>
2023-10-11 18:21:03 +0100

    implemented show command

---
+++ console.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+"""
+Basic console for HBNB  frontend usage
+"""
+import cmd
+from models.base_model import BaseModel, storage
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    Basic HBNB command interpreter
+    """
+
+    prompt = "(hbnb)"
+    def preloop(self) -> None:
+        self.classes = {"BaseModel": BaseModel}
+        return super().preloop()
+    def do_quit(self, line):
+        """Quit command to exit the program
+"""
+        exit(0)
+    
+    def emptyline(self):
+        """Handles empty line passed as command
+"""
+        pass
+
+    def do_EOF(self, line):
+        """cleanly exits command line interface on EOF signal (on `ctrl + D`)
+"""
+        exit(0)
+    
+    def do_create(self, line=""):
+        """creates a new object and saves it to the file
+"""
+        if line == "":
+            print("** class name missing **")
+            return
+        if line not in self.classes:
+            print("** class doesn't exist **")
+            return
+        model = self.classes.get(line)
+        obj = model()
+        print(obj.id)
+        obj.save()
+    
+    def do_show(self, line=""):
+        """Print the object of the class and id passed
+"""
+        args = line.split()
+        if line == "":
+            print("** class name missing **")
+            return
+        if args[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        objects = storage._FileStorage__object
+        id_exists = False
+        for x, y in objects.items():
+            # If a dictionary has id = args[1]: Create new object instance
+            # and print
+            if y.get('id') == args[1]:
+                model = self.classes.get(args[0])
+                # recreate instance from it's dictionary of attributes
+                new = model(**y)
+                print(new)
+                id_exists = True
+        if id_exists is False:
+            print("** no instance found **")
+        
+    def do_destroy(self, line=""):
+        """Destroy the object of the class and id passed
+"""
+        args = line.split()
+        if line == "":
+            print("** class name missing **")
+            return
+        if args[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        objects = storage._FileStorage__object
+        id_exists = False
+        for x, y in objects.items():
+            # If a dictionary has id = args[1]: Create new object instance
+            # and print
+            if y.get('id') == args[1]:
+                
+                id_exists = True
+        if id_exists is False:
+            print("** no instance found **")
+        
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
