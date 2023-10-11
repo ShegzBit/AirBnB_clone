bce553d924d6e4b93d067437284bf0b1b73ea9e9
Mahama Hamida <mahamahamida@gmail.com>
2023-10-11 16:13:48 +0000

    updated the base test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,10 +1,13 @@
-#!/usr/bin/python3
+#!/c/Users/maham/AppData/Local/Microsoft/WindowsApps/python
 """
     Tests for the base class
 """
 import unittest
 from models.base_model import BaseModel
 from datetime import datetime
+from unittest.mock import patch
+import io
+import time
 
 
 class TestBase(unittest.TestCase):
@@ -37,6 +40,7 @@ class TestBase(unittest.TestCase):
     def test_save_method(self):
         """tests the save method"""
         prev_updated_at = self.my_model.updated_at
+        time.sleep(1)
         self.my_model.save()
         new_updated_at = self.my_model.updated_at
         self.assertNotEqual(prev_updated_at, new_updated_at)
@@ -54,12 +58,23 @@ class TestBase(unittest.TestCase):
 
     def test_init_from_dict(self):
         """Test re-creating an instance from a dictionary representation"""
+        self.my_model.name = "My_First_Model"
+        self.my_model.my_number = 89
         model_dict = self.my_model.to_dict()
-        new_model = BaseModel(**model_dict)
+        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            new_model = BaseModel(**model_dict)
+            print(new_model)
+        expected_output = (
+            "[BaseModel] ({}) ".format(new_model.id) +
+            "{}".format(new_model.__dict__)
+        )
 
+
+        self.assertEqual(mock_stdout.getvalue().strip(), expected_output)
         self.assertEqual(self.my_model.id, new_model.id)
         self.assertEqual(self.my_model.created_at, new_model.created_at)
         self.assertEqual(self.my_model.updated_at, new_model.updated_at)
+        self.assertFalse(self.my_model is new_model)
 
 
 if __name__ == "__main__":
