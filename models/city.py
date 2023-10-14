de8ce86a1c24f91a373ae4546545ff5e41d5c82a
ShegzBit <feranmiawotubo16@gmail.com>
2023-10-14 12:42:48 +0100

    merging stage_branch to main

---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module contains City class for Airbnb console
+project
+"""
+from models.state import BaseModel
+
+
+class City(BaseModel):
+    """
+    City class for Airbnb project
+    """
+    state_id =""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/env/python3
+"""
+This module contains State class for Airbnb console
+project
+"""
+from models.user import BaseModel
+
+
+class State(BaseModel):
+    """
+    State object template for Airbnb console
+    project
+    """
+    name = ""
