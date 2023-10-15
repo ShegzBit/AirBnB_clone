0726eda7daf459f6200c6db68c32a9e3cc0d9e04
ShegzBit <feranmiawotubo16@gmail.com>
2023-10-15 01:01:45 +0100

    Implement proper documentation of modules and packages

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,6 @@
+"""
+models and object template used for AirBnB console
+"""
 from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
---
+++ models/engine/__init__.py
@@ -0,0 +1,3 @@
+"""
+The package containing Airbnb file storage
+"""
